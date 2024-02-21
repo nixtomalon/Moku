d4599353f94f317e7d6fb2dcd4aa76ab00ace579
macleanemorydunkin <macleanemorydunkin@gmail.com>
2024-02-21 02:42:25 +0000

    modified:   android/app/src/main/AndroidManifest.xml 	modified:   android/build.gradle 	new file:   lib/assets/logo.png 	modified:   lib/pages/about_page.dart 	modified:   lib/pages/notifications_page.dart 	modified:   macos/Flutter/GeneratedPluginRegistrant.swift 	modified:   pubspec.lock 	modified:   pubspec.yaml

--- lib/pages/about_page.dart
+++ lib/pages/about_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutPage extends StatelessWidget {
   const AboutPage({Key? key}) : super(key: key);
@@ -14,7 +15,66 @@ class AboutPage extends StatelessWidget {
         ),
       ),
       body: Center(
-        child: Text('Information about the app will be here'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            const SizedBox(height: 30),
+            Image.asset(
+              'lib/assets/logo.png',
+              width: MediaQuery.of(context).size.width * 0.15,
+              fit: BoxFit.contain,
+            ),
+            const SizedBox(height: 30),
+            const Divider(),
+            Flexible(
+              child: ListView(
+                children: [
+                  const ListTile(
+                    contentPadding: EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 8.0,
+                    ),
+                    title: Text("Version"),
+                    subtitle: Text("Preview 0.0.1"),
+                  ),
+                  ListTile(
+                    contentPadding: const EdgeInsets.symmetric(
+                      horizontal: 16.0,
+                      vertical: 0,
+                    ),
+                    title: const Text("What's new"),
+                    onTap: () async {
+                      const url = 'https://github.com/Maclean-D/Moku/releases';
+                      if (await canLaunchUrl(Uri.parse(url))) {
+                        await launchUrl(Uri.parse(url));
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Could not launch URL')),
+                        );
+                      }
+                    },
+                  ),
+                  // Add more ListTiles here for additional items
+                ],
+              ),
+            ),
+            IconButton(
+              // Assuming you're using a generic code icon or a custom GitHub icon
+              icon: Icon(Icons.code), // Change this icon to your GitHub icon
+              onPressed: () async {
+                const url = 'https://github.com/Maclean-D/Moku';
+                if (await canLaunchUrl(Uri.parse(url))) {
+                  await launchUrl(Uri.parse(url));
+                } else {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Could not launch URL')),
+                  );
+                }
+              },
+            ),
+            // Add more widgets here if needed
+          ],
+        ),
       ),
     );
   }
--- lib/pages/notifications_page.dart
+++ lib/pages/notifications_page.dart
@@ -1,7 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-class NotificationsPage extends StatelessWidget {
+class NotificationsPage extends StatefulWidget {
   const NotificationsPage({Key? key}) : super(key: key);
+
+  @override
+  _NotificationsPageState createState() => _NotificationsPageState();
+}
+
+class _NotificationsPageState extends State<NotificationsPage> {
+  late WebViewController _controller;
+
+  Future<void> _launchSettings() async {
+    final Uri settingsUri = Uri.parse('https://anilist.co/settings/notifications');
+    if (await canLaunchUrl(settingsUri)) {
+      await launchUrl(settingsUri);
+    } else {
+      throw 'Could not open $settingsUri';
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    // Initialize the WebViewController here if needed
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(onNavigationRequest: (NavigationRequest request) {
+          if (request.url.startsWith('https://anilist.co/settings/')) {
+            // Prevent navigation in WebView and launch externally if settings page is navigated to
+            _launchSettings();
+            return NavigationDecision.prevent;
+          }
+          return NavigationDecision.navigate;
+        }),
+      );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,9 +48,15 @@ class NotificationsPage extends StatelessWidget {
           icon: const Icon(Icons.arrow_back),
           onPressed: () => Navigator.of(context).pop(),
         ),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.settings),
+            onPressed: _launchSettings,
+          ),
+        ],
       ),
-      body: Center(
-        child: Text('Your notifications will appear here.'),
+      body: WebViewWidget(
+        controller: _controller..loadRequest(Uri.parse('https://anilist.co/notifications')),
       ),
     );
   }
